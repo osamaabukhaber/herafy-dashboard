2ce459d8e90aa31a6fc318e3d5b8a940ae5eede7
Minamaher-sw <mosadefmena185@gmail.com>
2025-08-02 22:13:46 +0300

    add StoreUpdateSroreComponent with HTML, CSS, and unit tests

---
+++ src/app/features/store/components/store-update-srore-component/store-update-srore-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-update-srore-component',
+  imports: [],
+  templateUrl: './store-update-srore-component.html',
+  styleUrl: './store-update-srore-component.css'
+})
+export class StoreUpdateSroreComponent {
+
+}
