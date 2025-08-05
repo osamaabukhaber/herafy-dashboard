2912f9403d7758948be5f9880ffba4fdef655773
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-08-05 20:39:47 +0300

    authintcation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './features/main-component/main-component';
-import {NotFoundComponent } from './shared/components/notfound/notfound';
+import { NotFoundComponent } from './shared/components/notfound/notfound';
 import { User } from './features/user/user';
 import { ProductListComponent } from './features/products/components/product-list/product-list.component';
 import { StoreComponent } from './features/store/components/store-component/store-component';
@@ -9,23 +9,32 @@ import { StoreUpdateSroreComponent } from './features/store/components/store-upd
 import { CategoryComponent } from './features/category-component/category-component';
 // import { ProductFormComponent } from './features/products/components/product-form/product-form.component';
 // import { ProductDetailComponent } from './features/products/components/product-detail/product-detail.component';
-
+import { AuthGuard } from './core/guards/auth.guard';
+import { AuthStatusResolver } from './core/resolvers/auth-status-resolver';
+import { Login } from './shared/login/login';
 export const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    /* resolve: { loggedIn: AuthStatusResolver }, */
+    canActivate: [AuthGuard], // optional now, could be removed
     children: [
       { path: 'user', component: User },
       { path: 'products', component: ProductListComponent },
-      { path:"store",component:StoreComponent ,title:"store"},
-      { path:"add-new-store", component:StoreAddedNewstoreComponent ,title:"add new store"},
-      { path: 'update/:id', component: StoreUpdateSroreComponent, title: "update store"},
-      {path: 'category', component: CategoryComponent}
-      // { path: 'products/create',   },
-      // { path: 'products/:id', component: ProductDetailComponent },
-      // { path: 'products/:id/edit',  }
+      { path: 'store', component: StoreComponent, title: 'store' },
+      {
+        path: 'add-new-store',
+        component: StoreAddedNewstoreComponent,
+        title: 'add new store',
+      },
+      {
+        path: 'update/:id',
+        component: StoreUpdateSroreComponent,
+        title: 'update store',
+      },
+      { path: 'category', component: CategoryComponent },
     ],
   },
-
+  { path: 'login', component: Login },
   { path: '**', component: NotFoundComponent, title: 'Not Found' },
 ];
